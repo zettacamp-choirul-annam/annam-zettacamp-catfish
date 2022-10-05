655c456958b7eedef8f6efbf512e90e9e94ff875
choirul.annam@zettacamp.pro <choirul.annam@zettacamp.pro>
2022-10-05 15:16:21 +0700

    done

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+const $main = document.querySelector(".main");
+
+fetch("assets/data.json").then(async resp => {
+    const body = await resp.json();
+    body.forEach(data => renderCard(data));
+});
+
+function renderCard(data) {
+    const $container = document.createElement("div");
+          $container.className = "card__container";
+          
+    let html = "";
+    
+    data.forEach(item => {
+        html += `
+            <div class="card">
+                <div class="card__image">
+                   <img src="${item.image}" alt="image">
+                </div>
+                <h2 class="card__title">${item.title}</h2>
+                <p class="card__description">${item.desc}</p>
+                <a class="card__link" href="">View Product</a>
+            </div>
+        `;
+    });
+    
+    $container.insertAdjacentHTML("beforeend", html);
+    $main.append($container);
+}
